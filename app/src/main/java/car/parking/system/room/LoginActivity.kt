05f919be54a034c89ec27f8c6e518a676f0df234
Jose <zebaptista26@gmail.com>
2022-12-12 19:45:43 +0000

    Login funcional - jose

--- app/src/main/java/car/parking/system/room/LoginActivity.kt
+++ app/src/main/java/car/parking/system/room/LoginActivity.kt
@@ -2,19 +2,66 @@ package car.parking.system.room
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.carparkingsystem.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import android.app.Activity
+import android.content.ContentValues.TAG
+import com.example.carparkingsystem.databinding.ActivityLoginBinding
 
 
 class LoginActivity : AppCompatActivity()
 {
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?)
     {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+
+        binding.btnSingin.setOnClickListener {
+            val email = binding.edEmail.text.toString()
+            val pass = binding.edPassword.text.toString()
+
+            if (email.isNotEmpty() && pass.isNotEmpty()) {
+
+                auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
+
+            }
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(auth.currentUser != null){
+            val intent = Intent(this, MenuActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     fun buttonMain(view: View) {
@@ -22,5 +69,5 @@ class LoginActivity : AppCompatActivity()
         startActivity(intent)
     }
 
-    
+
 }
--- app/src/main/java/car/parking/system/room/SobreProjetoActivity.kt
+++ app/src/main/java/car/parking/system/room/SobreProjetoActivity.kt
@@ -2,6 +2,7 @@ package car.parking.system.room
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,5 +13,10 @@ class SobreProjetoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.sobre_projeto)
+    }
+
+    fun buttonMain3(view: View) {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
